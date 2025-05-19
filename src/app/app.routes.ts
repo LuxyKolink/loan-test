2be77686c7419a117f2b638ba6bc69e7cc100a49
LuxyKolink <santiagolikeaboss22@gmail.com>
2025-05-19 00:11:25 -0500

    prueba en componente navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { UsersComponent } from './pages/users/users.component';
 import { AddUserComponent } from './pages/add-user/add-user.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/users', pathMatch: 'full' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
   { path: 'users', component: UsersComponent },
   { path: 'add-user', component: AddUserComponent },
-  { path: '**', redirectTo: '/users' }, // Catch-all route for any unmatched routes
+  { path: '**', redirectTo: '/home' }, // Catch-all route for any unmatched routes
 ];
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-
+  title: string = 'Loan Management';
+  links: { name: string; path: string }[] = [
+    { name: 'Users', path: '/users' },
+    { name: 'Contact', path: '/contact' },
+  ]
 }
